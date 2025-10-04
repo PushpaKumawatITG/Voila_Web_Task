17e4d94c1212c0cbfbe5d5d03e70afd1b2f4010d
PushpaKumawatITG <pushpakumawat@itgeks.com>
2025-10-04 18:30:10 +0530

    3rd commit

--- script.js
+++ script.js
@@ -3,13 +3,41 @@ const crosssearchbarbtn = document.getElementById("crosssearchbar");
 const searchbar = document.getElementById("searchbar");
 const MenuBar = document.getElementById("MenuBar");
 const menuToggle = document.getElementById("menuToggle");
+const addCartbtn = document.getElementById("addCartbtn");
+const cartPage = document.getElementById("cartPage");
+const crossCart = document.getElementById("crossCart");
 
-searchbtn.addEventListener("click", () => {
+searchbtn.addEventListener("click", (e) => {
+    e.stopPropagation();
     searchbar.classList.remove("displayNone");
+    document.body.style.overflow = "hidden";
 });
-crosssearchbarbtn.addEventListener("click", () => {
+crosssearchbarbtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    searchbar.classList.add("displayNone");
+    document.body.style.overflow = "auto";
+});
+document.addEventListener("click", () => {
     searchbar.classList.add("displayNone");
 });
+searchbar.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
+
+if(window.innerWidth >= 1024){
+    addCartbtn.addEventListener("click",(e) => {
+        e.stopPropagation();
+        cartPage.classList.remove("displayNone");
+        cartPage.classList.add("display");
+        cartPage.style.opacity = '1';
+        // document.body.style.overflow = "hidden";
+    });
+    crossCart.addEventListener('click',() => {
+        cartPage.classList.add("displayNone");
+        cartPage.classList.remove("display");
+        // document.body.style.overflow = "auto";
+    });
+}
 
 window.addEventListener("DOMContentLoaded", () => {
     if (window.innerWidth <= 1024) {
